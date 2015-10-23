b4e1e630e027935887b9cb7ace5ab577c1243b18
gonzamvita <gonzamv@gmail.com>
2015-10-23 18:46:36 +0200

    ex 1 & 2

--- javaScript/funciones/js/functions.js
+++ javaScript/funciones/js/functions.js
@@ -5,24 +5,28 @@
 // Complete the following function to get the letter linked to a spanish DNI
 // The functions receives an eight-digit number and returns a single letter.
 // The algorithm to calculate the letter is as follows:
-// The number is divided by 23 and the remainder is replaced by a letter determined by the following table: 
+// The number is divided by 23 and the remainder is replaced by a letter determined by the following table:
 //	 _____________________________________________________________
 //  | 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 |
 //	| T R W A G M Y F P D X  B  N  J  Z  S  Q  V  H  L  C  K  E  |
 //	|____________________________________________________________|
-// 
+//
 // You can use 'result' variable to store the resulting letter.
 // If your code works, the last three equalities will be "true"
-
 
 /*
 
 function dniLetter( dni ) {
+  if (typeof(dni) == 'number' && dni.toString().length == 8) {
+    var lockup = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    var result = '';
 
-	var lockup = 'TRWAGMYFPDXBNJZSQVHLCKE';
-	var result = '';
+    result = lockup.charAt(dni % 23);
 
     return result;
+  }else{
+    return 'Invalid parameter';
+  };
 }
 
 console.log( dniLetter( 12345678 ) === 'Z');
@@ -30,7 +34,6 @@ console.log( dniLetter( 34667892 ) === 'S');
 console.log( dniLetter( 92234488 ) === 'A');
 
 */
-
 
 //EXERCISE 2
 //
@@ -46,10 +49,9 @@ console.log( dniLetter( "A1234567" ) === 'Invalid parameter');
 
 */
 
-
 //EXERCISE 3
 //
-// Restore the comments marks (/* */) of the previous exercises. First two exercises have to be commented 
+// Restore the comments marks (/* */) of the previous exercises. First two exercises have to be commented
 // Uncomment the exercise code deleting /* and */ chars
 // Open the console and reload the index page to see the result of the code.
 // Write a function that takes an array of words and returns the length of the longest one.
@@ -74,7 +76,6 @@ console.log( findLongestWord( ["Red", "Blue", "Green"] ) === 5 );
 // You have to complete the calculateAverage function (it is a few lines below), and it has
 // to return the average of every number in a array instead of the "Not implemented yet" message.
 
-/*
 
 var array_lengths = [];
 array_lengths.push( findLongestWord( ["Richie", "Joanie", "Greg", "Marcia", "Bobby"]) );
@@ -90,12 +91,11 @@ function calculateAverage( array )
 	return( "Not implemented yet" )
 }
 
-// Now add more lengths to the array_lengths. You have to do it where <place to add more lines> is written. 
+// Now add more lengths to the array_lengths. You have to do it where <place to add more lines> is written.
 // You can use this structure: array_lengths.push( findLongestWord( [<add some elements>] ) );
 // Don't foget to add words to the array. The longer the better!
 // Now reload the page and see how changed the average.
 
-*/
 
 
 //EXERCISE 5
@@ -104,7 +104,7 @@ function calculateAverage( array )
 // Uncomment the exercise code deleting /* and */ chars
 // Now we are going to save the longest length of every array in a new array called array_lengths
 // Open the console and reload the index page to see the result of the code.
-// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. 
+// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it.
 // As a condition you must use the object freqCounter.
 // Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 
@@ -151,7 +151,7 @@ console.log( counter['f'] === 1 );
 
 /*
 
-function arrayAwesomenator( array ) 
+function arrayAwesomenator( array )
 {
 	array = deleteRubbish( array );
 	array = arrangeElements( array );
@@ -166,7 +166,7 @@ function deleteRubbish( array )
 {
 	//It receives an array and returns it without '*' chars
 	//Example:  deleteRubbish(['a',1,'*',5]) returns ['a',1,5]
-	
+
 }
 
 function arrangeElements( array )
@@ -186,7 +186,7 @@ function beautifyLetters( array )
 function beautifyNumbers( array )
 {
 	//It receives an array with numbers and letters and returns it with its numbers beautified. Letters remain unchanged
-	//Beautify process is to reduce a number into a single digit number by adding all its digits together: 
+	//Beautify process is to reduce a number into a single digit number by adding all its digits together:
 	// 123 = 6 because 1+2+3 = 6
 	// 9 = 9
 	// 9956 = 2 because 9+9+5+6 = 29 -> 2+9 = 11 -> 1+1 = 2
