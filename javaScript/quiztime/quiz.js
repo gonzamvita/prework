95c691305ec271587ba60243601d1d20c5412003
gonzamvita <gonzamv@gmail.com>
2015-10-23 20:01:12 +0200

    quiztime

---
+++ javaScript/quiztime/quiz.js
@@ -0,0 +1,107 @@
+var user = {}
+
+var responses = []
+
+function question1() {
+  var name = prompt('What is your name?')
+  user.name = name
+}
+
+question1();
+
+
+function question2() {
+
+  var firstQuestion = prompt('Does null === 0 ? (Yes or No)')
+
+// why do you need to convert the answer to lowercase?
+  if (firstQuestion.toLowerCase() === 'yes') {
+    firstQuestion = true
+  } else if (firstQuestion.toLowerCase() === 'no') {
+    firstQuestion = false
+  } else {
+// what if the user writes something other than yes or no?
+// they will have to answer the question again
+    alert("Please answer either Yes or No");
+    return question2();
+  }
+  responses.push(firstQuestion); // add the true or false value to the responses array
+}
+
+question2();
+
+
+function question3() {
+  var js = prompt('What was the original name for JavaScript: Java, LiveScript, JavaLive, or ScriptyScript?');
+  js = js.toLowerCase();
+  switch (js) {
+    case 'java':
+      alert('No, Java is another different language');
+      js = false;
+      responses.push(js);
+    break;
+
+    case 'livescript':
+      js = true;
+      responses.push(js);
+    break;
+
+    case 'javalive':
+      alert('Almost! I will give you one more chance');
+      secondchance();
+    break;
+
+    case 'scriptyscript':
+      alert('really?');
+      js = false;
+      responses.push(js);
+    break;
+
+    default:
+      js = false;
+      responses.push(js);
+    break;
+  }
+}
+
+function secondchance(){
+  question3();
+}
+
+question3();
+
+function evaluate(responsesArray) {
+
+// declare two variables to store the totals
+  var correct = 0;
+  var incorrect = 0;
+
+// populate the “totals” variables from the “responsesArray”
+
+  for (var i = responsesArray.length - 1; i >= 0; i--) {
+    if (responsesArray[i]){
+      correct++;
+    }else{
+      incorrect++;
+    };
+  };
+
+// save the “totals” variables inside the user object
+  user.correct = correct;
+  user.incorrect = incorrect;
+
+// call showResults
+  showResults()
+}
+
+function showResults() {
+  console.log("So, " + user.name + " you had " + user.correct + " correct answers, and " + user.incorrect + " incorrect answers.")
+  if (user.incorrect == 0) {
+    console.log("That's a perfect!")
+  }else{
+    console.log("Well done!")
+  };
+}
+
+// call the function, passing it the responses array
+evaluate(responses);
